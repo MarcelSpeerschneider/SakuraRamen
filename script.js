addd77fab078951021a2bdb675996fcebf826adc
Marcel Speerschneider <marcelspeerschneider@duck.com>
2023-06-25 12:46:35 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function translateGermanToEnglish() {
+
+    document.getElementById("nav-menu").innerHTML = "Menu";
+    document.getElementById("nav-locations").innerHTML = "LOCATIONS";
+    document.getElementById("nav-about").innerHTML = "ABOUT";
+    document.getElementById("headline-text-below").innerHTML = "THE BEST RAMEN RESTAURANT IN TOWN";
+    document.getElementById("pick-your-noodle").innerHTML = "WÄHLE DEINE NUDELN";
+    document.getElementById("pick-your-broth").innerHTML = "WÄHLE DEINE SOUCE";
+    document.getElementById("add-your-toppings").innerHTML = "WÄHLE DEINE BEILAGEN";
+    document.getElementById("pick-your-noodle").innerHTML = "Pick <br> Your Noodle";
+    document.getElementById("pick-your-broth").innerHTML = "Pick <br> Your Broth";
+    document.getElementById("add-your-toppings").innerHTML = "Add <br> Your Toppings";
+    document.getElementById("locations").innerHTML = "FIND US AT";
+}
+
+function translateEnglishToGerman() {
+
+    document.getElementById("nav-menu").innerHTML = "MENÜ";
+    document.getElementById("nav-locations").innerHTML = "ANFAHRT";
+    document.getElementById("nav-about").innerHTML = "ÜBER UNS";
+    document.getElementById("headline-text-below").innerHTML = "BESTES RAMEN RESTAURANT DER STADT";
+    document.getElementById("how-to-order").innerHTML = "WIE DU BESTELLST";
+    document.getElementById("pick-your-noodle").innerHTML = "Wähle Deine <br> Nudeln";
+    document.getElementById("pick-your-broth").innerHTML = "Wähle Deine <br> Souce";
+    document.getElementById("add-your-toppings").innerHTML = "Wähle Deine <br> Beilagen";
+    document.getElementById("our-menu").innerHTML = "UNSER MENÜ";
+    document.getElementById("locations").innerHTML = "WO DU UNS FINDEST";
+
+}
+
